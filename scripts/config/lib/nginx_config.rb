bea3edfc61c02b7933c541a5c661368de366092d
Ian Kinney <ian_kinney@mckinsey.com>
2023-03-02 14:10:21 -0500

    Adding configs from old buildpack

--- scripts/config/lib/nginx_config.rb
+++ scripts/config/lib/nginx_config.rb
@@ -6,11 +6,8 @@ class NginxConfig
   DEFAULT = {
     root: "public_html/",
     encoding: "UTF-8",
-    canonical_host: false,
     clean_urls: false,
     https_only: false,
-    basic_auth: false,
-    basic_auth_htpasswd_path: "/app/.htpasswd",
     worker_connections: 512,
     resolver: "8.8.8.8",
     logging: {
@@ -27,8 +24,7 @@ class NginxConfig
     json["root"] ||= DEFAULT[:root]
     json["encoding"] ||= DEFAULT[:encoding]
 
-    json["canonical_host"] ||= DEFAULT[:canonical_host]
-    json["canonical_host"] = NginxConfigUtil.interpolate(json["canonical_host"], ENV) if json["canonical_host"]
+    json["host_for_sendgrid"] = ENV["HOST_FOR_SENDGRID"]
 
     index = 0
     json["proxies"] ||= {}
@@ -50,10 +46,6 @@ class NginxConfig
 
     json["clean_urls"] ||= DEFAULT[:clean_urls]
     json["https_only"] ||= DEFAULT[:https_only]
-
-    json["basic_auth"] = true unless ENV['BASIC_AUTH_USERNAME'].nil?
-    json["basic_auth"] ||= DEFAULT[:basic_auth]
-    json["basic_auth_htpasswd_path"] ||= DEFAULT[:basic_auth_htpasswd_path]
 
     json["routes"] ||= {}
     json["routes"] = NginxConfigUtil.parse_routes(json["routes"])
